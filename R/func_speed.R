45de6084114095c58ea7683271e5d44dacfe4db9
Kyle Coombs <kyle.g.coombs@gmail.com>
2024-06-03 17:44:44 -0400

    added back in plotting

--- R/func_speed.R
+++ R/func_speed.R
@@ -691,3 +691,60 @@ lpdid_dt <- function(df, window = c(NA, NA), y,
     dt<-lpdid_dt_prep(df, window = window, y = y,unit_index = unit_index, time_index = time_index,treat_status = treat_status,cluster = cluster,controls = controls,controls_t = controls_t,outcome_lags = outcome_lags,reweight = reweight,pmd = pmd, pmd_lag = pmd_lag,composition_correction = composition_correction,pooled = pooled,nonabsorbing_lag = nonabsorbing_lag)
     return(lpdid_dt_reg(dt, unit_index = unit_index, time_index = time_index,cluster = cluster,controls = controls,controls_t = controls_t,reweight = reweight))
 }
+
+
+#' Plot LP-DiD Event Study dt
+#'
+#' Plotting LP-DiD event study parameter estimates and confidence intervals. Uses output from data.table.
+#' @param reg An object generated via the lpdid function.
+#' @param conf The confidence level (1-alpha) desired for confidence intervals. Default is 0.95 (95% confidence internval)
+#' @param segments A boolean (TRUE or FALSE) value for whether confidence intervals should be generated.  Default is TRUE.
+#' @param add A boolean (TRUE or FALSE) value for whether the user wants to add to an existing figure or generate a new one.  The default is FALSE, meaning a new plot.
+#' @param xlab The text belonging on the x axis.
+#' @param ylab The text belonging on the y axis.
+#' @param ylim A vector of length two that determines the minimum and maximum of the y-axis. Default scales to fit the figure being plotted.
+#' @param main The text belonging as the figure's title.
+#' @param x.shift A numeric value that will shift the event study estimates along the x-axis.
+#' @param pch A numeric value corresponding to the point's shape.
+#' @param cex A numeric value corresponding to the size of the point.
+#' @param col The color the points (and confidence intervals) should be.
+#' @param opacity A numeric value between 0 and 1 that corresponds to the opacity of the color.
+#' @return An event study plot.
+#' @export
+plot_lpdid_dt <- function(reg, conf = .95,                  segments = TRUE, add = FALSE,
+                       xlab = NULL, ylab = NULL,
+                       ylim = NULL,
+                       main = "", x.shift = 0,
+                       pch = 19, cex = 1, col = "black", opacity = 1){
+
+#   if(nrow(reg$coeftable) != length(reg$window)) stop("coeftable and window are not the same length.  It is likely that pooled=TRUE in the lpdid function.  An event study cannot be plotted when pooled=TRUE.")
+  coeftable <- reg$coefs
+  conf_z <- abs(qnorm((1-conf)/2))
+  uCI <- coeftable$Estimate + conf_z*coeftable$`Std. Error`
+  lCI <- coeftable$Estimate - conf_z*coeftable$`Std. Error`
+
+  if(!add){
+
+    if(is.null(ylim)){
+
+      ylim <- c(min(lCI, na.rm = T), max(uCI, na.rm = T))
+    }
+
+    plot(coeftable$t + x.shift, coeftable$Estimate, las = 1, pch = pch,
+         cex = cex, main = main, col = scales::alpha(col, opacity),
+         ylim = ylim,
+         xlim = range(coeftable$t, na.rm = T),
+         xlab = ifelse(is.null(xlab), "Time to Treatment", xlab),
+         ylab = ifelse(is.null(ylab),
+                       paste0("Coefficient Estimate and ", conf*100, "% Confidence Interval"),
+                       ylab))
+    abline(h = 0, v = -1, lty = 2)
+  } else {
+
+    points(coeftable$t + x.shift, coeftable$Estimate, pch = pch,
+           cex = cex, col = scales::alpha(col, opacity))
+  }
+
+  if(segments) segments(x0 = coeftable$t + x.shift,
+                        y0 = uCI, y1 = lCI, col = scales::alpha(col, opacity))
+}
